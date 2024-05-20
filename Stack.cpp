7b2c035e30f85bf199c44f069f046beff88c56a8
Дмитрий Науменко <Krasawa34rus@yandex.ru>
2024-05-20 11:06:56 +0300

    Test Message 123

--- Stack.cpp
+++ Stack.cpp
@@ -1,10 +1,13 @@
 #include "Stack.h"
 
-void Add(int a, int b) {
+int Add(int a, int b) {
 	return a + b;
+}
 
-
-void Sub(int a, int b) {
+int Sub(int a, int b) {
 	return a - b;
+}
 
+int Mult(int a) {
+	return 1;
 }
